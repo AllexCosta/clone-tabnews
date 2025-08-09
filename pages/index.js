4df31b509fa86baa4e1bb1209cf1251bfc203471
Allex Valério Costa <allex.developer@gmail.com>
2025-08-09 18:34:20 +0000

    Ajuste no index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    <h1>Aline, eu te adoro e for reciproco, dá uma risadinha!!! ;)</h1>
+    return <h1>Aline, eu te adoro e se for reciproco, dá uma risadinha!!! ;)</h1>
 }
 
 export default Home;
