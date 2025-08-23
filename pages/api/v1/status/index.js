6a4d00491424bd23fb1767bcedfbb8407c532268
Allex Valério Costa <allex.developer@gmail.com>
2025-08-23 19:30:47 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,38 @@
 import database from "infra/database.js";
+import { Connection } from "pg";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "OK" });
+  // Atribui data e hora atual convertida em ISO para a constante `updatedAd`
+  const updatedAt = new Date().toISOString();
+  /*
+    Atribui a váriavel `queryPgVersion` o resultado da query "SELECT version();"
+    Atribui a constante `pgVersion` somente o valor `PostregsSQL (Versão)`
+  */
+  let queryPgVersion = await database.query("SHOW server_version;");
+  const pgVersion = queryPgVersion.rows[0].server_version;
+  /*
+    Atribui a váriavel `queryMaxConnections` o resultado da query "SHOW max_connections;"
+    Atribui a constante `maxConnections` somente o valor `Máximo de Conexões`
+  */
+  let queryMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnections = queryMaxConnections.rows[0].max_connections;
+  /*
+    Atribui a váriavel `queryUsedConnections` o resultado da query "SELECT count(*) FROM pg_stat_activity;"
+    Atribui a constante `usedConnections` somente a contagem da quantidade de conexões
+  */
+  const dataBaseName = process.env.POSTGRES_DB;
+  let queryUsedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dataBaseName],
+  });
+  const usedConnections = queryUsedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    postgres_version: pgVersion,
+    max_connections: parseInt(maxConnections),
+    used_connections: usedConnections,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,3 +2,42 @@ test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
+
+//Apresenta data e hora atual
+test("Apresenta Data e Hora Atualizada", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+});
+
+//Apresenta a Versão do Postgres
+test("Apresenta a versão do postgres", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  let stringifyPgVersion = JSON.stringify(responseBody.postgres_version);
+  const parsedPgVersion = stringifyPgVersion
+    .replace(/\\"/g, "")
+    .replace(/"/g, "");
+  expect(responseBody.postgres_version).toBe(parsedPgVersion);
+});
+
+//Apresenta o Máximo de Conexões
+test("Apresenta o Máximo de Conexões", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  expect(responseBody.max_connections).toBe(100);
+});
+
+//Apresenta Conexões Utilizadas
+test("Apresenta Conexões Utilizadas", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  expect(responseBody.used_connections).toBe(1);
+});
